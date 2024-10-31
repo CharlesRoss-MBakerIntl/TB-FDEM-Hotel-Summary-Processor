9b8e09c563fa40ae954c490b8d35573c7f19aff0
Charles Ross <charles.ross@mbakerintl.com>
2024-10-31 14:17:55 -0800

    Update project-table pull file and env

--- process_table.py
+++ process_table.py
@@ -11,13 +11,17 @@ from query_package import get_query_package
 
 #Load Environment Variables
 load_dotenv()  # Load environment
-token = os.getenv('GITHUBTOKEN')  # GitHub toke
-access = os.getenv('ACCESS') # AWS access token
-secret = os.getenv('SECRET') # AWS secret token
-username = os.getenv('USER') # AWS RDS username
-password = os.getenv('PASSWORD') # AWS RDS password
-server = os.getenv('SERVER') # AWS RDS server
-db = os.getenv('DB') # AWS RDS database
+token = os.getenv('GITHUBTOKEN')    # GitHub toke
+access = os.getenv('ACCESS')    # AWS access token
+secret = os.getenv('SECRET')    # AWS secret token
+username = os.getenv('USER')    # AWS RDS username
+password = os.getenv('PASSWORD')    #AWS RDS password
+server = os.getenv('SERVER')    # AWS RDS server
+db = os.getenv('DB')    # AWS RDS database
+bucket = os.getenv('BUCKET')    # AWS RDS bucket name
+project_folder = os.getenv('PROJECTFOLDER')     # AWS S3 Project Folder Path
+active_folder = os.getenv('ACTIVEFOLDER')    # AWS S3 Project Active Folder Path
+archive_folder = os.getenv('ARCHIVEFOLDER')     # AWS S3 Project Archive Folder Path
 
 
 
@@ -29,13 +33,28 @@ def process_table(event, context):
 
     # Access RDS Functions via GitHub
     try:
-        rds_functions_url = 'https://raw.githubusercontent.com/CharlesRoss-MBakerIntl/Tidal-Basin-Functions/main/rds_connector.py' # Set url to python file of github
-        rds_functions = fetch_function(rds_functions_url, token) # Pull function from github using requests
-        exec(rds_functions) # Execute the file
+        rds_connector_url = 'https://raw.githubusercontent.com/CharlesRoss-MBakerIntl/Tidal-Basin-Functions/main/rds_connector.py' # Set url to python file of github
+        rds_connector = fetch_function(rds_connector_url, token) # Pull function from github using requests
+        exec(rds_connector) # Execute the file
 
     except Exception as e:
         print(traceback.print_exc())
         raise Exception(f"Failed to Pull RDS Functions from GitHub") from e
+    
+
+
+    #----------------------------------------------------------------
+
+    # Access RDS Functions via GitHub
+    try:
+        s3_manager_url = 'https://raw.githubusercontent.com/CharlesRoss-MBakerIntl/Tidal-Basin-Functions/main/rds_connector.py' # Set url to python file of github
+        s3_manager = fetch_function(s3_manager_url, token) # Pull function from github using requests
+        exec(s3_manager) # Execute the file
+
+    except Exception as e:
+        print(traceback.print_exc())
+        raise Exception(f"Failed to Pull RDS Functions from GitHub") from e
+    
 
 
     #----------------------------------------------------------------
@@ -71,11 +90,41 @@ def process_table(event, context):
         print(traceback.print_exc())
         raise Exception(f"Failed to Produce RDS Table from RDSTablePull in rds_connector.py") from e 
     
+
+
+    #----------------------------------------------------------------
+
+    # Add Active File to Project S3
+    try:
+        rds = RDSTablePull(conn, cursor, query_package) # Create Instance of RDS Table
+
+    except Exception as e:
+        print(traceback.print_exc())
+        raise Exception(f"Failed to Produce RDS Table from RDSTablePull in rds_connector.py") from e 
+    
+
+
+
+     #----------------------------------------------------------------
+
+    # Add Archive Versions to Project S3
+    try:
+        rds = RDSTablePull(conn, cursor, query_package) # Create Instance of RDS Table
+
+    except Exception as e:
+        print(traceback.print_exc())
+        raise Exception(f"Failed to Produce RDS Table from RDSTablePull in rds_connector.py") from e 
     
     
+    
+
     # TODO implement
     return {
         
         'statusCode': 200,
         'body': json.dumps('Hotel Summaries Table Processed')
     }
+
+
+
+
